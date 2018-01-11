7bf0e62e4a26a89943255093ae971aedd6b98ae1
sai.lu@itdev.xiangguang.com <sai.lu@itdev.xiangguang.com>
2018-01-11 15:49:04 +0800

    Dashboard improved 🎨 .

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -6,7 +6,8 @@
       <el-col :span="6" v-for="(o, index) in 1" :key="o" :offset="index > 0 ? 1 : 0">
         <el-card :body-style="{ padding: '0px' }">
           <div style="padding: 14px;font-size: 24px;line-height: 46px;">
-            <svg-icon icon-class="user"/><span> 用户名 : {{name}}</span>
+            <svg-icon icon-class="user"/>
+            <span> 用户名 : {{name}}</span>
             <div class="bottom clearfix">
               <span v-for='role in roles' :key='role.$index'>
                 <svg-icon icon-class="role"/> 用户角色 : {{role.DisplayName}}
@@ -20,27 +21,28 @@
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
+  import { mapGetters } from 'vuex'
 
-export default {
-  name: 'dashboard',
-  computed: {
-    ...mapGetters([
-      'name',
-      'roles'
-    ])
+  export default {
+    name: 'dashboard',
+    computed: {
+      ...mapGetters([
+        'name',
+        'roles'
+      ])
+    }
   }
-}
 </script>
 
 <style rel="stylesheet/scss" lang="scss" scoped>
-.dashboard {
-  &-container {
-    margin: 30px;
+  .dashboard {
+
+    &-container {
+      margin: 30px;
+    }
+    &-text {
+      font-size: 30px;
+      line-height: 46px;
+    }
   }
-  &-text {
-    font-size: 30px;
-    line-height: 46px;
-  }
-}
 </style>
