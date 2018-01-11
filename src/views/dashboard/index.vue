0d1d85d08b070b05ef53e64b49bd3e1a82dfafe5
sai.lu@itdev.xiangguang.com <sai.lu@itdev.xiangguang.com>
2018-01-11 15:02:25 +0800

    Dashboard improved 🎨 .

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,7 +1,19 @@
 <template>
   <div class="dashboard-container">
-    <div class="dashboard-text">name:{{name}}</div>
-    <div class="dashboard-text">role:<span v-for='role in roles' :key='role.$index'>{{role}}</span></div>
+    <!--<div class="dashboard-text">name:{{name}}</div>-->
+    <!--<div class="dashboard-text">role:<span v-for='role in roles' :key='role.$index'>{{role}}</span></div>-->
+    <el-row>
+      <el-col :span="6" v-for="(o, index) in 1" :key="o" :offset="index > 0 ? 1 : 0">
+        <el-card :body-style="{ padding: '0px' }">
+          <div style="padding: 14px;font-size: 24px;line-height: 46px;">
+            <svg-icon icon-class="user"/><span> 用户名 : {{name}}</span>
+            <div class="bottom clearfix">
+              <svg-icon icon-class="role"/><span v-for='role in roles' :key='role.$index'> 用户角色 : {{role.DisplayName}}</span>
+            </div>
+          </div>
+        </el-card>
+      </el-col>
+    </el-row>
   </div>
 </template>
 
