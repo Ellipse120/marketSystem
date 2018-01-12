660d2e4ce3b1348b2783dcf29d94b64d17dcdf6f
sai.lu@itdev.xiangguang.com <sai.lu@itdev.xiangguang.com>
2018-01-12 18:36:51 +0800

    代码改进 🎨

--- config/dev.env.js
+++ config/dev.env.js
@@ -3,6 +3,5 @@ var prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  // BASE_API: '"https://easy-mock.com/mock/5950a2419adc231f356a6636/vue-admin"'
   BASE_API: '"http://192.168.125.63:12345/api"',
 })
