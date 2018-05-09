1825cd414e190e060248cc8f037332c8c1bac52b
James Lu <ellipse120@gmail.com>
2018-05-09 11:07:55 +0800

    add require.context() for import vuex modules.

--- src/store/index.js
+++ src/store/index.js
@@ -1,30 +1,13 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import app from './modules/app'
-import user from './modules/user'
-import permission from './modules/permission'
-import errorLog from './modules/errorLog'
-import bloomConfig from './modules/bloomConfig'
-import codeConfig from './modules/codeConfig'
-import marketData from './modules/market-data'
-import upload from './modules/upload'
 import getters from './getters'
-import enums from './modules/enums'
+
+import modules from './modules'
 
 Vue.use(Vuex)
 
 const store = new Vuex.Store({
-  modules: {
-    app,
-    user,
-    permission,
-    errorLog,
-    bloomConfig,
-    codeConfig,
-    marketData,
-    enums,
-    upload
-  },
+  modules,
   getters
 })
 
